3f18c745a64411cfece40a7b9310a68adaa3b463
Andras Bali <andras.bali@gmail.com>
2020-06-22 16:25:00 +0200

    Rewrite ItemViewer component to Reach Hooks and styled-components.

--- src/ItemViewer.js
+++ src/ItemViewer.js
@@ -1,109 +1,95 @@
-import React from 'react';
-import {apiCall, formatDate} from './lib/util';
-import './ItemViewer.css';
-import Hotkeys from 'react-hot-keys';
+import React, { useState, useEffect, useRef } from 'react';
+import { apiCall, formatDate } from './lib/util';
+import styled from 'styled-components';
+import { useHotkeys } from 'react-hotkeys-hook';
 
-class ItemViewer extends React.Component {
-	constructor(props) {
-	  super(props);
-	  this.state = {
-		item: null
-	  };
-	  this.fetch = this.fetch.bind(this);
-	  this.handleStar = this.handleStar.bind(this);
-	  this.topRef = React.createRef();
-	}
-	onKeyDown(keyName, e, handle) {
-		// Workaround: Pane component cannot be referenced otherwise by refs or ids
-		const div = document.getElementsByClassName('Pane horizontal Pane2')[1];
-		switch(keyName) {
-			case "f":
-				this.handleStar();
-				break;
-			case "v":
-				window.open(this.state.item.url, '_blank');
-				break;
-			case "c":
-				window.open(this.state.item.comments_url || this.state.item.url, '_blank')
-				break;
-			case 'pagedown':
-				div.scrollTop += div.clientHeight;
-				e.preventDefault();
-				break;
-			case 'pageup':
-				div.scrollTop -= div.clientHeight;
-				e.preventDefault();
-				break;
-			default: 
-		}
-	}
-	componentDidMount() {
-	  this.fetch();
-	}
-	componentDidUpdate(prevProps) {
-	  if (prevProps.currentItem !== this.props.currentItem) {
-		this.fetch();
-	  }
-	  if (this.topRef.current) { 
-		this.topRef.current.scrollIntoView();
-	  }
-	}
-	fetch() {
-	  if (!this.props.currentItem) {
-		this.setState({item: null})
-		return;
-	  }
-	  apiCall('entries/' + this.props.currentItem, this.props.errorHandler)
-	  .then(i => this.setState({item: i},
-		e => {}));
-	}
-	handleStar() {
-		apiCall('entries/' + this.state.item.id + '/bookmark', this.props.errorHandler, {})
-		.then(() => setTimeout(function() { this.fetch(); }.bind(this), 100),
-		e => {});
-	}
-	render() {
-	  const item = this.state.item;
-	  if (!item) {
-		return <div />;
-	  }
-	  const content = {__html: item.content };
-	  return (
-		<Hotkeys 
-		 keyName="f,v,c,pagedown,pageup"
-		 allowRepeat="true" 
-		 onKeyDown={this.onKeyDown.bind(this)}>
-		  <div ref={this.topRef}>
-		    <div className="itemheader">
-			  <table className="itemheader">
-				<tbody>
-					<tr>
-						<td className="title">
-							<a href={item.url} target="_blank" rel="noopener noreferrer">{item.title}</a>
-							&nbsp;
-							<a href={item.comments_url || item.url} title="Comments" style={{'textDecoration': 'none'}} target="_blank"  rel="noopener noreferrer">
-								<span role="img" aria-label="comments">&#128172;</span>
-							</a>
-						</td>
-						<td rowSpan="2" className="star">
-							<div className="star" onClick={this.handleStar} title="Toggle star">
-					    	{item.starred ? String.fromCharCode(9733) : String.fromCharCode(9734) }
-		  					</div>
-						</td>
-					</tr>
-					<tr>
-						<td className="controls">
-							{ item.author ? item.author : item.feed.title }, { formatDate(item.published_at) }
-						</td>
-					</tr>
-				</tbody>
-			  </table>
-		    </div>
-		    <div className="itemcontent" dangerouslySetInnerHTML={content} />
-		  </div>
-		</Hotkeys>
-		);
-	}
-  }
+const ItemHeader = styled.div`
+	padding: 2px;
+	background-color: lightgoldenrodyellow;
+	border-bottom: 1px solid lightgrey;
+	margin-left: 2px;
+	margin-right: 2px;
+	`;
 
-  export default ItemViewer;
+const ItemHeaderContent = styled.div`
+	display: inline-block;
+	`;
+
+const ItemContent = styled.div`
+	padding: 5px
+	`;
+
+const StarButton = styled.div`
+	display: inline-block;
+	float: right;
+	font-size: 20px;
+	&:hover {
+		cursor: pointer
+	}
+	`;
+
+function linkNewTab(title, link, stripStyle) {
+	return (<a href={link} 
+		target="_blank"
+		rel="noopener noreferrer"
+		style={ stripStyle && {'textDecoration': 'none'} }>
+			{title}
+			</a>)
+}
+
+function ItemViewer(props) {
+	const [item, setItem] = useState();
+	const [starToggle, setStarToggle] = useState(false);
+	const topRef = useRef();
+
+	useHotkeys('f', () => toggleStar(), [item]);
+	useHotkeys('v', () => window.open(item.url, '_blank'), [item]);
+	useHotkeys('c', () => window.open(item.comments_url || item.url, '_blank'), [item]);
+	useHotkeys('pagedown', (e) => { 
+		e.preventDefault();
+		const el = document.getElementsByClassName('Pane horizontal Pane2')[1];
+		el.scrollTop += el.clientHeight;
+	});
+	useHotkeys('pageup', (e) => { 
+		e.preventDefault();
+		const el = document.getElementsByClassName('Pane horizontal Pane2')[1];
+		el.scrollTop -= el.clientHeight;
+	});
+
+	useEffect(() => {
+		const fetchItem = async () => {
+			if (props.currentItem) {
+				const result = await apiCall('entries/' + props.currentItem, props.errorHandler);
+				setItem(result);
+				topRef.current.scrollIntoView();
+			}
+		};
+		fetchItem();
+	}, [props.currentItem, props.errorHandler, starToggle]);
+
+	const toggleStar = async () => {
+		await apiCall('entries/' + item.id + '/bookmark', props.errorHandler, {});
+		setStarToggle(!starToggle);
+	};
+
+	return !item ? null : (
+		<div ref={topRef}>
+			<ItemHeader>
+				<ItemHeaderContent>
+					{ linkNewTab(item.title, item.url) }
+					&nbsp;
+					{ linkNewTab(<span role="img" aria-label="comments">&#128172;</span>, item.comments_url || item.url, true) }
+					<br/>
+					{ item.author || item.feed.title }, { formatDate(item.published_at) }
+				</ItemHeaderContent>
+				<StarButton title="Toggle star" onClick={toggleStar}>
+					{ String.fromCharCode(item.starred ? 9733 : 9734) }
+		  		</StarButton>
+			</ItemHeader>
+			<ItemContent dangerouslySetInnerHTML={{__html: item.content}} />
+		</div>
+	)
+
+}
+
+export default ItemViewer;
